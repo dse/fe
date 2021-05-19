2c8c87c19a17a594f42bf76be4e7e0df36a9bf7c
Darren Embry <dembry@rfxtechnologies.com>
2021-05-19 15:21:26 -0400

    bug fixes and performance improvements

--- share/fe/shell-echo.sh
+++ share/fe/shell-echo.sh
@@ -3,31 +3,62 @@
 declare -i shell_echo_column=0
 declare -i shell_echo_space=0
 declare -i shell_echo_first=1
+declare -i shell_echo_next_newline=0
+declare -i shell_echo_comment=0
+
 shell-echo-raw () {
     local i
     local -i next_column
     local -i length
+    local -i newline
     for i ; do
         length="${#i}"
         next_column=$(( shell_echo_column + length + shell_echo_space ))
-        if (( !shell_echo_first && next_column > 76 )) ; then
+        newline=0
+
+        if (( shell_echo_next_newline )) ; then
+            newline=1
+        elif (( !shell_echo_first && next_column > 76 )) ; then
+            newline=1
+        elif (( as_flat )) && [[ "${i}" = "\\!" ]] ; then
+            newline=1
+        elif (( as_flat )) && [[ "${i}" = "-exec" ]] ; then
+            newline=1
+        fi
+
+        if (( newline )) ; then
             echo " \\"
-            echo        -n "    ${i}"
-            shell_echo_column=$(( 4 + length ))
+            shell_echo_column=0
+            if (( shell_echo_comment )) ; then
+                echo -n "# "
+                shell_echo_column+=2
+            fi
+            echo -n "    ${i}"
+            shell_echo_column+=$(( 4 + length ))
             shell_echo_space=1
             shell_echo_first=0
+            shell_echo_next_newline=0
         else
             if (( shell_echo_space )) ; then
                 echo -n " ${i}"
             else
+                if (( shell_echo_comment )) ; then
+                    echo -n "# "
+                    shell_echo_column+=2
+                fi
                 echo -n "${i}"
             fi
             shell_echo_column+=$(( length + shell_echo_space ))
             shell_echo_space=1
             shell_echo_first=0
+            shell_echo_next_newline=0
+        fi
+        if (( as_flat )) && [[ "${i}" = "\\)" ]] ; then
+            shell_echo_next_newline=1
         fi
     done
 }
+
 shell-echo () {
     local i ii
     for i ; do
@@ -37,12 +68,13 @@ shell-echo () {
                 ii="\\${i}";;
             '{}')
                 ii="${i}";;
-            *[^-./:+_A-Za-z0-9]*) # anything with a character not in that set, i.e., a character that needs to be quoted or escaped
+            *[^-+,./:=@_A-Za-z0-9]*) # anything with a character not in that set, i.e., a character that needs to be quoted or escaped
                 ii="${i@Q}";;
         esac
         shell-echo-raw "${ii}"
     done
 }
+
 shell-echo-nl () {
     shell_echo_column=0
     shell_echo_first=1
