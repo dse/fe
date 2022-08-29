52384b4ddc1e59732944e406c3b34686eecd8707
Darren Embry <dse@webonastick.com>
2022-08-29 13:38:23 -0400

    simplify command echoing

--- share/fe/shell-echo.sh
+++ share/fe/shell-echo.sh
@@ -1,26 +1,5 @@
 # -*- mode: sh; sh-shell: bash -*-
 
 shell-echo () {
-    local arg
-    local space=0
-    for arg in "$@" ; do
-        (( space )) && echo -n " "
-        shell-echo-arg-n "${arg}"
-        space=1
-    done
-    echo
-}
-
-shell-echo-arg-n () {
-    local arg="$1"
-    case "${arg}" in
-        '(')                     echo -n "\\(";;
-        ')')                     echo -n "\\)";;
-        '!')                     echo -n "\\!";;
-        ';')                     echo -n "\\;";;
-        "{}")                    echo -n "{}";;
-        "+")                     echo -n "+";;
-        *[^+,-./0-9:=@A-Z_a-z]*) echo -n "${arg@Q}";;
-        *)                       echo -n "${arg}";;
-    esac
+    echo "$@"
 }
