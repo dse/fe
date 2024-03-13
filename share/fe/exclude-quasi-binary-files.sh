9955049b0e94929aab32cbb7ec32eaea5bbc173b
Darren Embry <dse@webonastick.com>
2024-03-12 22:11:51 -0400

    add quasi-binary file exclusion list

---
+++ share/fe/exclude-quasi-binary-files.sh
@@ -0,0 +1,8 @@
+# -*- mode: sh; sh-shell: bash -*-
+exclude_files '*.doc'
+exclude_files '*.xls'
+exclude_files '*.ppt'
+exclude_files '*.pdf'
+exclude_files '*.ps'
+exclude_files '*.sfd'           # FontForge fonts
+exclude_files '*.shar'
