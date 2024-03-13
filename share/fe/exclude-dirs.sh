fca43f016049911b55d631c371c623ecfbca86c4
Darren Embry <dse@webonastick.com>
2024-03-12 22:04:02 -0400

    separate out the exclusion lists

---
+++ share/fe/exclude-dirs.sh
@@ -0,0 +1,25 @@
+# -*- mode: sh; sh-shell: bash -*-
+
+exclude_dirs .git
+exclude_dirs node_modules
+exclude_dirs _cacache
+exclude_dirs .node-gyp
+exclude_dirs vendor
+exclude_dirs cache
+exclude_dirs '.cpan'
+exclude_dirs 'npm-cache'
+exclude_dirs '.svn'
+exclude_dirs 'SCCS'
+exclude_dirs 'RCS'
+exclude_dirs 'CVS'
+exclude_dirs 'MCVS'
+exclude_dirs '.src'
+exclude_dirs '.hg'
+exclude_dirs '.bzr'
+exclude_dirs '_MTN'
+exclude_dirs '_darcs'
+exclude_dirs '{arch}'
+exclude_dirs '.sass-cache'
+exclude_dirs 'elpa'
+exclude_dirs 'zip-cache'
+exclude_dirs 'nytprof'
