0e9cc7ea960ed4c68183cc55977bc14d2ed4247d
Darren Embry <dse@webonastick.com>
2025-07-15 01:02:49 -0400

    fix inconsistent sets of files (a la -.js, -.html, -.css)

---
+++ share/fe/exclude-sets.sh
@@ -0,0 +1,34 @@
+# -*- sh-shell: bash -*-
+
+declare -a file_set__html=(
+    '*.xhtml'
+    '*.html'
+    '*.htm'
+    '*.php'
+)
+
+declare -a file_set__css=(
+    '*.css'
+    '*.scss'
+)
+
+declare -a file_set__js=(
+    '*.js'
+    '*.mjs'
+    '*.cjs'
+    '*.jsx'
+    '*.es6'
+    '*.es'
+    '*.iced'
+    '*.liticed'
+    '*.iced.md'
+    # '*.cs'
+    '*.coffee'
+    '*.litcoffee'
+    '*.coffee.md'
+    '*.ts'
+    '*.tsx'
+    '*.ls'
+    '*.sjs'
+    '*.eg'
+)
